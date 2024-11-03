8429567488435baee4554716c1e4caee97f06d0f
smalladi <165831061+msyd6@users.noreply.github.com>
2024-11-03 13:58:40 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('learn-more-btn').addEventListener('click', function () {
+    const hiddenContent = document.getElementById('hidden-content');
+    hiddenContent.style.display = hiddenContent.style.display === 'block' ? 'none' : 'block';
+});
