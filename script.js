a5c3a3aa51255e4d232ea4c47806f548fca60b6d
smalladi <165831061+msyd6@users.noreply.github.com>
2024-11-11 00:00:40 -0500

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('learn-more-btn').addEventListener('click', function () {
-    const hiddenContent = document.getElementById('hidden-content');
-    hiddenContent.style.display = hiddenContent.style.display === 'block' ? 'none' : 'block';
-});
